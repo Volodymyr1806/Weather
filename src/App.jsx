49c014dfd6f748fa29f02bbfff5f7f98dc5e7035
Volodymyr Oleksiienko <oleksienkovladimir50@gmail.com>
2022-02-20 21:19:41 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.scss';
+import { Route, Switch } from 'react-router-dom';
+import { FormOfSearch } from './components/FormOfSearch';
+import { ListOfCities } from './components/ListOfCities';
+import { DetailesOfCityWeather } from './components/DetailesOfCityWeather';
 
 export const App = () => (
-  <div className="starter">
-    React starter pack
+  <div className="App">
+    <Switch>
+      <Route path="/" exact>
+        <section className="App__home">
+          <h1 className="App__home_title title">
+            Check the weather in your city
+          </h1>
+          <FormOfSearch />
+          <div className="App__home_cities">
+            <ListOfCities />
+          </div>
+        </section>
+
+      </Route>
+      <Route path="/:city" component={DetailesOfCityWeather} />
+
+      <p>Not found page</p>
+    </Switch>
   </div>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import { store } from './store/index';
 import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
